df7c5c3dd87232c28b1967ac7465d36f1942406b
mahsa_naderkhah92 <mahsa.naderkhah92@gmail.com>
2023-06-22 10:46:22 +0330

    Add required factories and seeders

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'account_number' => $this->faker->unique()->bankAccountNumber,
+            'balance' => $this->faker->randomFloat(2, 100, 10000),
+            'user_id' => User::factory(),
+        ];
+    }
+}
