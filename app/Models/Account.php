b49dc2b87921d79a481bc0aebe1d5008e938b0ad
mahsa_naderkhah92 <mahsa.naderkhah92@gmail.com>
2023-06-22 10:49:23 +0330

    Add models

---
+++ app/Models/Account.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['account_number', 'balance'];
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
