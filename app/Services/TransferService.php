baee54b728a0e28393c56f3f9d8d98f808b11bd1
mahsa_naderkhah92 <mahsa.naderkhah92@gmail.com>
2023-06-22 16:31:40 +0330

    Add sms messages for sender and receiver

--- app/Services/TransferService.php
+++ app/Services/TransferService.php
@@ -8,6 +8,8 @@ use App\Repositories\AccountRepository;
 use App\Repositories\TransactionRepository;
 use App\Services\Notifier\NotificationStrategyFactory;
 use App\Services\Notifier\Notifier;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Lang;
 
 class TransferService
 {
@@ -25,15 +27,26 @@ class TransferService
         $this->accountRepository->addMoney($receiveAccount->id, $amount);
         $this->transactionRepository->addTransaction($senderCard->id, $receiverCard->id, $amount);
 
-        $this->notifyUsers($senderAccount->user, $receiveAccount->user);
+        $this->notifyUsers($senderAccount->user, $receiveAccount->user, $amount);
     }
 
-    private function notifyUsers(User $sender, User $receiver): void
+    private function notifyUsers(User $sender, User $receiver, int $amount): void
     {
         $strategy = NotificationStrategyFactory::create();
         $this->notifier->setStrategy($strategy);
 
-        $this->notifier->notify($sender->mobile, 'Hello, this is a test message for sender');
-        $this->notifier->notify($receiver->mobile, 'Hello, this is a test message for receiver');
+        $this->notifier->notify($sender->mobile, $this->getSenderMessage($amount));
+        $this->notifier->notify($receiver->mobile, $this->getReceiverMessage($amount));
+    }
+
+    private function getSenderMessage(int $amount): string
+    {
+        return Lang::get('transfer.sender_message', ['amount' => $amount, 'datetime' => (Carbon::now())->format('Y-m-d H:i:s')]);
+    }
+
+
+    private function getReceiverMessage(int $amount): string
+    {
+        return Lang::get('transfer.receiver_message', ['amount' => $amount, 'datetime' => (Carbon::now())->format('Y-m-d H:i:s')]);
     }
 }
